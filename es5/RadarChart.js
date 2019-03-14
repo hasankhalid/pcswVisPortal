a499fd5642eb0ce331631ebf1280a46237002bef
hasankhalid <hasankhalid940@gmail.com>
2019-03-14 17:29:21 +0500

    radar chart prompt added

--- es5/RadarChart.js
+++ es5/RadarChart.js
@@ -43,7 +43,7 @@ function computeValues(d, i) {
 var RadarChart = {
   draw: function (id, d, options) {
     var cfg = {
-      radius: 5.5,
+      radius: 4.5,
       w: 600,
       h: 600,
       factor: 1,
